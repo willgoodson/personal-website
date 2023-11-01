ecf88bfb6e6039ff00cde2558026d6bdec73b721
William Goodson <goodsonwill@gmail.com>
2023-10-31 23:36:39 -0500

    Added title and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <style>
     @import 'purecss/build/pure.css';
     @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-
 </style>
 
+<script>
+    import icon from '$lib/images/favicon.ico'
+</script>
+<svelte:head>
+    <link rel="icon" href={icon}>
+    <title>William Goodson</title>
+</svelte:head>
 <slot />
